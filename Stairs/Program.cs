a21f8217b8a9196d9cf50792917a05f80ac7b9d3
polvictor <poltorak.victor@gmail.com>
2022-04-23 18:32:14 +0300

    Add project files.

---
+++ Stairs/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Stairs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Pls input number of stairs: ");
+            int n = Convert.ToInt32(System.Console.ReadLine());
+            Console.WriteLine("\nWe are drawing stairs with " + n + " steps.\n");
+            DrawStairs(n);            
+
+            static void DrawStairs(int step)
+            {
+                for(int i = 0; i < step; i++)
+                {
+                    int space = 2 * i;
+                    for (int j = 0; j < space; j++)
+                    {
+                        Console.Write(" ");
+                    }
+                        Console.WriteLine("***");
+                    space = 2 * (i + 1);
+                    for (int j = 0; j < space; j++)
+                    {
+                        Console.Write(" ");
+                    }
+                        Console.WriteLine("*");                    
+                }
+            }
+
+        }
+    }
+}
